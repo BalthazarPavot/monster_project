32114fa37e5aeab494c76d0780c945b9fa6592cb
Balto <balthazar.pavot@hotmail.fr>
2017-04-28 18:06:38 +0200

    feat(heavy client: Context): creation
    Creation of the context singleton class that contains the global context of the application.

---
+++ heavy_client/src/metadata/Context.java
@@ -0,0 +1,55 @@
+package metadata;
+
+public class Context {
+
+	static public Context singleton = new Context();
+
+	private ErrorManager errorManager = null;
+	private boolean running = true;
+
+	private Context() {
+		this(null);
+	}
+
+	private Context(ErrorManager errorManager) {
+		if (errorManager == null)
+			this.errorManager = ErrorManager.singleton;
+		else
+			this.errorManager = errorManager;
+	}
+
+	public void setError(Exception error) {
+		try {
+			setError(error, false);
+		} catch (Exception e) {
+			// should never ocure
+			e.printStackTrace();
+			System.out.println("An error occurend in the error handler!");
+		}
+	}
+
+	public void setError(Exception error, Boolean raise) throws Exception {
+		this.errorManager.setError(error);
+		if (raise) {
+			setRunning(false);
+			errorManager.throwError();
+		} else
+			errorManager.silenceError();
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void loadDefaultConfiguration() {
+	}
+
+	public void loadConfiguration(String[] args) {
+
+	}
+
+}
