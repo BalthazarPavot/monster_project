0179aa07d067202749f94f4fe0ca61c8993307cc
Balto <balthazar.pavot@hotmail.fr>
2017-04-28 18:08:50 +0200

    feat(heavy client: MonsterProject): creation (main)
    Creation of the class containing the "main" method.

---
+++ heavy_client/src/MonsterProject.java
@@ -0,0 +1,55 @@
+
+/**
+ * 
+ */
+
+import metadata.Context;
+import screens.Screen;
+import screens.ScreenGenerator;
+
+/**
+ * @author lain
+ *
+ */
+public class MonsterProject {
+
+	/**
+	 * Launch the mainloop giving the first screen id.
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		Context.singleton.loadConfiguration(args);
+		mainloop(ScreenGenerator.MAIN_MENU_SCREEN);
+	}
+
+	/**
+	 * Create the game context and the error manager, load the config and create
+	 * the game screen generator giving the context. <br />
+	 * Launch the mainloop that prepare the current screen, make it run, get the
+	 * new game screen id and loop again while the player does not decide to
+	 * quit.
+	 * 
+	 * @param gameScreenID
+	 * @throws Exception
+	 */
+	public static void mainloop(int screenID) throws Exception {
+		ScreenGenerator screenGenerator;
+		Screen screen;
+		Context context;
+
+		context = Context.singleton;
+		screenGenerator = new ScreenGenerator(context);
+		do {
+			screenGenerator.prepareScreen(screenID);
+			screen = screenGenerator.getScreen();
+			if (screen == null)
+				context.setError(new Exception(String.format("Could not create asked screen with id %d.", screenID)),
+						false);
+			screenID = screen.run();
+		} while (context.isRunning() && screenID != ScreenGenerator.QUIT_SCREEN);
+		System.exit(0);
+	}
+
+}
