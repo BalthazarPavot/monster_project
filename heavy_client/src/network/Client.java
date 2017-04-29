f611fef2967ec22303d709b81ac062867c7ddea0
Balto <balthazar.pavot@hotmail.fr>
2017-04-29 23:36:53 +0200

    feat(heavy client:client): +post and get
    The client is now able to do posts and gets.

--- heavy_client/src/network/Client.java
+++ heavy_client/src/network/Client.java
@@ -1,6 +1,76 @@
 package network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+import metadata.Context;
+
 public class Client {
+
+	static private String USER_AGENT = "HeavyClient1.0 - Monster Project";
+
+	private String sendGetRequest(String url) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url);
+		StringBuffer result = null;
+
+		request.addHeader("User-Agent", USER_AGENT);
+		HttpResponse response;
+		try {
+			response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result.toString();
+	}
+
+	private String sendPostRequest(String url, HashMap<String, String> parameters) throws UnsupportedEncodingException {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(url);
+		StringBuffer result = null;
+
+		post.setHeader("User-Agent", USER_AGENT);
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
+		urlParameters.add(new BasicNameValuePair("cn", ""));
+		urlParameters.add(new BasicNameValuePair("locale", ""));
+		urlParameters.add(new BasicNameValuePair("caller", ""));
+		urlParameters.add(new BasicNameValuePair("num", "12345"));
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+		HttpResponse response;
+		try {
+			response = client.execute(post);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result.toString();
+	}
 
 	public boolean sendServerLoginRequest(String login, String password) {
 		return true;
