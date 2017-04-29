3e21ae94129bfb8bda5663137d59536f3c84eb6c
Balto <balthazar.pavot@hotmail.fr>
2017-04-29 20:37:02 +0200

    feat(heavy client:project): +getters setters
    Add setters and getters for the prject's attribute.

--- heavy_client/src/user/Project.java
+++ heavy_client/src/user/Project.java
@@ -11,8 +11,68 @@ public class Project {
 	private Boolean ReadOther = false ;
 	private Boolean loaded = false ;
 
+	public void loadProject () {
+		loaded = true ;
+	}
+
 	public Boolean isLoaded () {
 		return loaded ;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getWriteUser() {
+		return WriteUser;
+	}
+
+	public void setWriteUser(Boolean writeUser) {
+		WriteUser = writeUser;
+	}
+
+	public Boolean getWriteGroup() {
+		return WriteGroup;
+	}
+
+	public void setWriteGroup(Boolean writeGroup) {
+		WriteGroup = writeGroup;
+	}
+
+	public Boolean getWriteOther() {
+		return WriteOther;
+	}
+
+	public void setWriteOther(Boolean writeOther) {
+		WriteOther = writeOther;
+	}
+
+	public Boolean getReadUser() {
+		return ReadUser;
+	}
+
+	public void setReadUser(Boolean readUser) {
+		ReadUser = readUser;
+	}
+
+	public Boolean getReadGroup() {
+		return ReadGroup;
+	}
+
+	public void setReadGroup(Boolean readGroup) {
+		ReadGroup = readGroup;
+	}
+
+	public Boolean getReadOther() {
+		return ReadOther;
+	}
+
+	public void setReadOther(Boolean readOther) {
+		ReadOther = readOther;
 	}
 
 }
