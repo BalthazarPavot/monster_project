6e61decafca586d45f447e60b00c3a82c14d56a3
Balto <balthazar.pavot@hotmail.fr>
2017-04-29 20:28:19 +0200

    feat(heavy client:user): +(login/logout)
    The user can log in and out.
    Anonymous login is now in a static attribute.

--- heavy_client/src/user/User.java
+++ heavy_client/src/user/User.java
@@ -2,10 +2,25 @@ package user;
 
 public class User {
 
+	public static final String ANONYMOUS = "anonymous";
 	private boolean connected = false ;
+	private String login = ANONYMOUS ;
 
 	public boolean isConnected () {
 		return connected ;
+	}
+
+	public void setConnected (String login) {
+		this.login = login ;
+		connected = login.equals(ANONYMOUS) == false ;
+	}
+
+	public void logout() {
+		setConnected (ANONYMOUS) ;
+	}
+
+	public String getLogin () {
+		return login ;
 	}
 
 }
