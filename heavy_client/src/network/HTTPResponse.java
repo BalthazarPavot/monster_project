fb85bb8f5e2b615ecc68228f525e026fd4dc9279
Balto <balthazar.pavot@hotmail.fr>
2017-05-03 08:22:04 +0200

    feat(heavy client:HTTPResponse): creation
    Creation of a very simple class to represent an http response with its error code and the response content.

---
+++ heavy_client/src/network/HTTPResponse.java
@@ -0,0 +1,43 @@
+package network;
+
+public class HTTPResponse {
+
+	static String DEFAULT_CONTEXT = "";
+	static Integer DEFAULT_ERROR_CODE = 200;
+
+	private String content = DEFAULT_CONTEXT;
+	private Integer errorCode = DEFAULT_ERROR_CODE;
+
+	public HTTPResponse() {
+		this(DEFAULT_ERROR_CODE, DEFAULT_CONTEXT);
+	}
+
+	public HTTPResponse(Integer errorCode) {
+		this(errorCode, DEFAULT_CONTEXT);
+	}
+
+	public HTTPResponse(String content) {
+		this(DEFAULT_ERROR_CODE, content);
+	}
+
+	public HTTPResponse(Integer errorCode, String content) {
+		this.content = content;
+		this.errorCode = errorCode;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(Integer errorCode) {
+		this.errorCode = errorCode;
+	}
+}
