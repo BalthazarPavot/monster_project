452a495b8655d84030c4b9a32858c5718065e560
Balto <balthazar.pavot@hotmail.fr>
2017-05-06 06:38:53 +0200

    feat(heavy client:modelManager): user can be individually mapped
    Creation of a function to map users individually.

--- heavy_client/src/metadata/ModelManager.java
+++ heavy_client/src/metadata/ModelManager.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 
 import javax.xml.bind.*;
 
+import model.User;
 import network.HTTPResponse;
 
 public class ModelManager {
@@ -21,6 +22,8 @@ public class ModelManager {
 			response = Context.singleton.client.getLoggedUsers(project_id);
 			if (response.hasErrorCode(200)) {
 				users = extractUserList(response);
+			} else {
+				System.err.println("Bad http error code while connected users gathering");
 			}
 		} catch (UnsupportedEncodingException e) {
 			Context.singleton.setSilencedError(e);
@@ -35,6 +38,19 @@ public class ModelManager {
 			jc = JAXBContext.newInstance(UserList.class);
 			Unmarshaller unmarshaller = jc.createUnmarshaller();
 			return (UserList) unmarshaller.unmarshal(new StringReader(response.getContent()));
+		} catch (JAXBException e) {
+			Context.singleton.setSilencedError(e);
+		}
+		return null;
+	}
+
+	public User mapUser(String content) {
+		JAXBContext jc = null;
+
+		try {
+			jc = JAXBContext.newInstance(model.User.class);
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			return (model.User) unmarshaller.unmarshal(new StringReader(content));
 		} catch (JAXBException e) {
 			Context.singleton.setSilencedError(e);
 		}
