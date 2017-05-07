5c90c082a710cfd0f194f96204fd489e19183db6
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-05-07 17:03:26 +0200

    feat(DatabaseHandler.java): add checkIfExists method and change scope of constants

--- src/monster_project/DatabaseHandler.java
+++ src/monster_project/DatabaseHandler.java
@@ -1,52 +1,68 @@
-package monster_project ;
+package monster_project;
 
-import java.sql.* ;
+import java.sql.*;
 
 public class DatabaseHandler {
-	
-	private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver" ;
-	private static final String DB_HOST = "localhost" ;
-	private static final String DB_NAME = "monster_project" ;
-	private static final String DB_PORT = "3306" ;
-	private static final String DB_USER = "root" ;
-	private static final String DB_PASSWORD = ":p" ;
-	private static final String SERVER_URL = "jdbc:mysql://" + DB_HOST + ":" +
-											 DB_PORT + "/" + DB_NAME ;
 
+	private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+	protected static final String DB_HOST = "localhost";
+	protected static final String DB_NAME = "monster_project";
+	protected static final String DB_PORT = "3306";
+	protected static final String DB_USER = "root";
+	protected static final String DB_PASSWORD = "trouduc";
+	protected static final String SERVER_URL = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 
-	private Connection conn ;
+	private Connection conn;
 
-	public DatabaseHandler () {	
-		System.out.println("Connecting to MySQL server ...") ;
+	public DatabaseHandler() {
 		try {
-			Class.forName (DRIVER_CLASS) ;
-			this.conn = DriverManager.getConnection (SERVER_URL,
-													DB_USER, DB_PASSWORD) ;
+			Class.forName(DRIVER_CLASS);
+			this.conn = DriverManager.getConnection(SERVER_URL, DB_USER, DB_PASSWORD);
 		} catch (SQLException e) {
-			e.printStackTrace() ;
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace() ;
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	public void printAllRecord (String query) throws SQLException {
-		Statement stmt = this.conn.createStatement() ;
-		ResultSet rs = stmt.executeQuery (query) ;
-		ResultSetMetaData rsmd = rs.getMetaData() ;
-		int columnNumber = rsmd.getColumnCount() ;
-		while ( rs.next() ) {
-			for (int i=1 ; i < columnNumber ; i++ ) {
-				System.out.print(rs.getString(i) + " ") ;
+
+	public void printAllRecords(String query) throws SQLException {
+		Statement stmt = this.conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnNumber = rsmd.getColumnCount();
+		while (rs.next()) {
+			for (int i = 1; i <= columnNumber; i++) {
+				System.out.print(rs.getString(i) + " ");
 			}
-			System.out.println() ;
+			System.out.println();
 		}
-		rs.close() ;
-		stmt.close() ;
+		rs.close();
+		stmt.close();
 	}
-	
-	public void closeConnection () throws SQLException {
-			this.conn.close() ;
+
+	public void executeQuery(String query) throws SQLException {
+		Statement stmt = this.conn.createStatement();
+		stmt.executeUpdate(query);
+		stmt.close();
+	}
+
+	public boolean executeChecking(String query) throws SQLException {
+		boolean out = false;
+		Statement stmt = this.conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnNumber = rsmd.getColumnCount();
+		while (rs.next()) {
+			for (int i = 1; i <= columnNumber; i++) {
+				out = Boolean.valueOf(rs.getString(i)) != null;
+			}
+		}
+		rs.close();
+		stmt.close();
+		return out;
+	}
+
+	public void closeConnection() throws SQLException {
+		this.conn.close();
 	}
 }
-
