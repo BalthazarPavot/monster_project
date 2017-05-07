5447ee523eed4ef3417446c85435efdfa66db378
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-05-07 17:05:03 +0200

    refractor(TestDatabaseHandler.java): try to add an user and a group

--- src/monster_project/TestDatabaseHandler.java
+++ src/monster_project/TestDatabaseHandler.java
@@ -1,12 +1,28 @@
 package monster_project;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class TestDatabaseHandler {
 
-	public static void main(String[] args) throws SQLException {
-		DatabaseHandler dbh = new DatabaseHandler() ;
-		
-		dbh.printAllRecord ("SELECT * FROM user ; ") ;
+	public static void main(String[] args) throws SQLException, IOException {
+		DatabaseServer ds = new DatabaseServer();
+
+		/*
+		 * String initDb = "mysql -u " + DatabaseHandler.DB_USER +
+		 * " --password=" + DatabaseHandler.DB_PASSWORD + " -e " +
+		 * "\"source /home/tilito/workspace/monster_project/database_creation.sql\""
+		 * ;
+		 */
+
+		// Runtime runtime = Runtime.getRuntime();
+		// runtime.exec(initDb);
+		// System.out.println(initDb);
+		System.out.println("Début");
+		System.out.println(ds.addUser("OOOOOOO", "111111111", "2222222222"));
+		System.out.println(ds.addGroup("fe749668-8fc4-41b8-85ce-209ea498de7e", "grpp"));
+		System.out.println("Fin");
+
+		ds.closeConnection();
 	}
 }
