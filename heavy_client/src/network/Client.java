4204ee66ad1c0b3e912a927c6602733e74bceae7
Balto <balthazar.pavot@hotmail.fr>
2017-05-08 13:57:57 +0200

    feat(heavy client:Client): +(NewGroup, ConfProject, OpenProject, CreateProject)
    The client is now able to send requests to create new groups, configures projects, open and create projects.

--- heavy_client/src/network/Client.java
+++ heavy_client/src/network/Client.java
@@ -18,6 +18,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
 import metadata.Context;
+import model.Permission;
 
 public class Client {
 
@@ -25,6 +26,10 @@ public class Client {
 	static private String LOGIN_URL = "user/login";
 	static private String REGISTER_URL = "user/new";
 	static private String LOGGED_USERS_URL = "project/users/{{project_id}}/{{format}}";
+	static private String PROJECT_CREATION_URL = "document/new";
+	static private String PROJECT_OPEN_URL = "document/{{owner_name}}/{{document_name}}";
+	static private String NEW_GROUP_URL = "group/new";
+	
 	private String protocol = "http";
 
 	public HTTPResponse sendGetRequest(String url) {
@@ -66,7 +71,7 @@ public class Client {
 			urlParameters.add(new BasicNameValuePair(key, parameters.get(key)));
 		}
 		post.setEntity(new UrlEncodedFormEntity(urlParameters));
-		HttpResponse response = null ;
+		HttpResponse response = null;
 		try {
 			response = client.execute(post);
 			http_response.setErrorCode(response.getStatusLine().getStatusCode());
@@ -102,48 +107,111 @@ public class Client {
 				.replace("{{format}}", "xml");
 	}
 
+	private String getCreateProjectURL() {
+		return String.format("%s/%s", getBaseURL(), PROJECT_CREATION_URL);
+	}
+
+	private String getOpenProjectURL(String ownerName, String projectName) {
+		return String.format("%s/%s", getBaseURL(),
+				PROJECT_OPEN_URL.replace("{{owner_name}}", ownerName).replace("{{document_name}}", projectName));
+	}
+
+	private String getCreateGroupURL() {
+		return String.format("%s/%s", getBaseURL(), NEW_GROUP_URL);
+	}
+
 	public HTTPResponse getLoggedUsers(String project_id) throws UnsupportedEncodingException {
 		HashMap<String, String> parameters = new HashMap<>();
 
 		if (Context.singleton.user.isConnected()) {
-			parameters.put("id", Context.singleton.user.getId()) ;
-			parameters.put("type", "heavy") ;
-			parameters.put("ip", Context.singleton.getClientIP()) ;
-			parameters.put("port", Context.singleton.getClientPort().toString()) ;
+			parameters.put("id", Context.singleton.user.getId());
+			parameters.put("type", "heavy");
+			parameters.put("ip", Context.singleton.getClientIP());
+			parameters.put("port", Context.singleton.getClientPort().toString());
 			return sendPostRequest(getConnectedUsersURL(project_id), parameters);
 		} else {
-			HTTPResponse response = new HTTPResponse();
-			response.setErrorCode(401);
-			return response ;
+			return new HTTPResponse(401);
 		}
 	}
 
 	public HTTPResponse sendServerLoginRequest(String login, String password) {
 		HashMap<String, String> parameters = new HashMap<>();
 
+		parameters.put("login", login);
+		parameters.put("password", password);
 		try {
-			parameters.put("login", login);
-			parameters.put("password", password);
 			return sendPostRequest(getLoginURL(), parameters);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return new HTTPResponse(401);
 	}
 
 	public HTTPResponse sendServerRegisterRequest(String login, String password, String email) {
 		HashMap<String, String> parameters = new HashMap<>();
 
+		parameters.put("login", login);
+		parameters.put("password", password);
+		parameters.put("password_verif", password);
+		parameters.put("email", password);
 		try {
-			parameters.put("login", login);
-			parameters.put("password", password);
-			parameters.put("password_verif", password);
-			parameters.put("email", password);
 			return sendPostRequest(getRegisterURL(), parameters);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return new HTTPResponse(401);
+	}
+
+	public HTTPResponse sendServerCreateProjectRequest(String name) {
+		HashMap<String, String> parameters = new HashMap<>();
+
+		if (Context.singleton.user.isConnected()) {
+			parameters.put("name", name);
+			parameters.put("user_id", Context.singleton.user.getId());
+			try {
+				return sendPostRequest(getCreateProjectURL(), parameters);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return new HTTPResponse(401);
+	}
+
+	public HTTPResponse sendServerOpenProjectRequest(String ownerName, String projectName) {
+		if (Context.singleton.user.isConnected())
+			return sendGetRequest(getOpenProjectURL(ownerName, projectName));
+		return new HTTPResponse(401);
+	}
+
+	public HTTPResponse sendServerConfProjectRequest(Permission perms) {
+		HashMap<String, String> parameters = new HashMap<>();
+		String xmlPermissions ;
+
+		if (Context.singleton.user.isConnected() && Context.singleton.project.isLoaded()) {
+			xmlPermissions = Context.singleton.modelManager.unmapPermission (perms) ;
+			parameters.put("permissions", xmlPermissions);
+			try {
+				return sendPostRequest(getCreateProjectURL(), parameters);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return new HTTPResponse(401);
+	}
+
+	public HTTPResponse sendServerNewGroupRequest(String groupName) {
+		HashMap<String, String> parameters = new HashMap<>();
+
+		if (Context.singleton.user.isConnected()) {
+			parameters.put("user_id", Context.singleton.user.getId());
+			parameters.put("name", groupName);
+			try {
+				return sendPostRequest(getCreateGroupURL(), parameters);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		}
+		return new HTTPResponse(401);
 	}
 
 }
