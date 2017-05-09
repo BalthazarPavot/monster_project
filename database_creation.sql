1783bfa1b89b590a18d165c1db6f0f6161f120fc
Pelegrin-Romain <pelegrinromain@gmail.com>
2017-05-09 18:12:56 +0200

    feat(database_creation.sql): add of jointures table between group, permission and document

--- database_creation.sql
+++ database_creation.sql
@@ -86,45 +86,19 @@ CREATE TABLE `permission_document_user` (
 	FOREIGN KEY(`id_user`) REFERENCES `user`(`id`)
 ) ;
 
+CREATE TABLE `permission_document_group` (
+	`id_permission` VARCHAR(90) NOT NULL,
+	`id_document` VARCHAR(90) NOT NULL,
+	`id_group` VARCHAR(90) NOT NULL,
+	FOREIGN KEY(`id_permission`) REFERENCES `permission`(`id`),
+	FOREIGN KEY(`id_document`) REFERENCES `document`(`id`),
+	FOREIGN KEY(`id_group`) REFERENCES `group`(`id`)
+) ;
+
+
 CREATE TABLE `message_user` (
 	`id_user` VARCHAR(90) NOT NULL,
 	`id_message` VARCHAR(90) NOT NULL,
 	FOREIGN KEY(`id_user`) REFERENCES `user`(`id`),
 	FOREIGN KEY(`id_message`) REFERENCES `message`(`id`)
 ) ;
-
-
-SELECT "Filling tables ..." AS 'Message : ' ;
-INSERT INTO `user` (`id`, `pseudo`, `password`, `email`) VALUES (1, "Miloune", "azertyuiop", "orefefikj@ij") ;
-INSERT INTO `user` (`id`, `pseudo`, `password`, `email`) VALUES (2, "ToBal", "qsdfghjklm", "oikefj@ij") ;
-INSERT INTO `user` (`id`, `pseudo`, `password`, `email`) VALUES (3, "Moi", "wxcvbn", "oizdzdkj@ij");
-
-INSERT INTO `group` (`id`, `id_owner`, `name`) VALUES (1, 1, "MiTo") ;
-INSERT INTO `group` (`id`, `id_owner`, `name`) VALUES (2, 2, "ToMoi") ;
-
-INSERT INTO `user_in_group` (`id_user`, `id_group`) VALUES (1, 1) ;
-INSERT INTO `user_in_group` (`id_user`, `id_group`) VALUES (2, 1) ;
-INSERT INTO `user_in_group` (`id_user`, `id_group`) VALUES (2, 2) ;
-INSERT INTO `user_in_group` (`id_user`, `id_group`) VALUES (3, 2) ;
-
-INSERT INTO `document` (`id`, `id_owner`, `name`) VALUES (1, 3, "Mondocument") ;
-
-INSERT INTO `document_text` (`id`, `id_parent`, `content`) VALUES (1, 1, 
-"Badass") ;
-
-INSERT INTO `permission` (`id`, `r`, `w`) VALUES (1, 1, 1) ;
-INSERT INTO `permission` (`id`, `r`, `w`) VALUES (2, 1, 0) ;
-
-INSERT INTO `permission_document_user` (`id_permission`, `id_document`, `id_user`)
-	VALUES (1, 1, 3) ;
-INSERT INTO `permission_document_user` (`id_permission`, `id_document`, `id_user`)
-	VALUES (2, 1, 1) ;
-INSERT INTO `permission_document_user` (`id_permission`, `id_document`, `id_user`)
-	VALUES (2, 1, 2) ;
-
-/*SELECT `user`.`pseudo`, `document`.`name`, `permission`.`r`, `permission`.`w`, `permission`.`x`, `content`
-	FROM `user`, `document`, `permission`, `permission_document_user`, `document_text` 
-	WHERE `user`.`id` = `permission_document_user`.`id_user` 
-	AND `document`.`id` = `permission_document_user`.`id_document` 
-	AND `permission`.`id` = `permission_document_user`.`id_permission` 
-	AND `document`.`id` = `document_text`.`id_parent` ;*/
